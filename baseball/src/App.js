71ee2837383088d09a0a42f2923da1405d636063
Nerissa Taylor <taylornerissa@hotmail.com>
2019-09-05 18:50:39 -0400

    still working on it

---
+++ baseball/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Dashboard from './components/Dashboard';
+import './App.css';
+import Display from './components/Display';
+
+function App() {
+  return (
+    <div className="App">
+      <Dashboard />
+      <Display />
+    </div>
+  );
+}
+
+export default App;
